ce59616e89763f7bd327e8fb224810db5e6e2017
Peter Williams <pwil3058@gmail.com>
2016-08-30 15:25:59 +1000

    dialogue: add an AskerMixin to simplify the asking of simple questions
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- aipoed/gui/dialogue.py
+++ aipoed/gui/dialogue.py
@@ -78,3 +78,35 @@ class Dialog(Gtk.Dialog, BusyIndicator):
     def __init__(self, **kwargs):
         Gtk.Dialog.__init__(self, **kwargs)
         BusyIndicator.__init__(self, kwargs.get("parent", None))
+
+class QuestionDialog(Dialog):
+    def __init__(self, question="", explanation="", **kwargs):
+        Dialog.__init__(self, **kwargs)
+        self.set_skip_taskbar_hint(True)
+        self.set_destroy_with_parent(True)
+        grid = Gtk.Grid()
+        self.vbox.add(grid)
+        image = Gtk.Image()
+        image.set_from_stock(Gtk.STOCK_DIALOG_QUESTION, Gtk.IconSize.DIALOG)
+        grid.add(image)
+        q_label = Gtk.Label()
+        q_label.set_markup("<big><b>" + question + "</b></big>")
+        q_label.set_justify(Gtk.Justification.LEFT)
+        q_label.set_line_wrap(True)
+        grid.attach_next_to(q_label, image, Gtk.PositionType.RIGHT, 1, 1)
+        if explanation:
+            e_label = Gtk.Label(explanation)
+            e_label.set_justify(Gtk.Justification.FILL)
+            e_label.set_line_wrap(True)
+            grid.attach_next_to(e_label, q_label, Gtk.PositionType.BOTTOM, 1, 1)
+        self.show_all()
+
+CANCEL_OK_BUTTONS = (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OK, Gtk.ResponseType.OK)
+NO_YES_BUTTONS = (Gtk.STOCK_NO, Gtk.ResponseType.NO, Gtk.STOCK_YES, Gtk.ResponseType.YES)
+
+class AskerMixin:
+    def ask_question(self, question, explanation="", buttons=CANCEL_OK_BUTTONS):
+        dialog = QuestionDialog(parent=self.get_toplevel(), buttons=buttons, question=question, explanation=explanation)
+        response = dialog.run()
+        dialog.destroy()
+        return response

353414909898edf95f1fb116336fc59841356b7a
Peter Williams <pwil3058@gmail.com>
2016-09-28 11:21:11 +1000

    wd_actions: split into two and place in pm and scm modules as approptiate
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- aipoed/pm/gui/actions.py
+++ aipoed/pm/gui/actions.py
@@ -17,16 +17,12 @@
 Workspace status action groups
 '''
 
-from gi.repository import Gtk
+from ... import enotify
+from ... import pm
 
-from .. import enotify
-from .. import pm
-from .. import scm
-
-from . import actions
+from ...gui import actions
 
 AC_NOT_IN_PM_PGND, AC_IN_PM_PGND, AC_IN_PM_PGND_MUTABLE, AC_IN_PM_PGND_MASK = actions.ActionCondns.new_flags_and_mask(3)
-AC_NOT_IN_SCM_PGND, AC_IN_SCM_PGND, AC_IN_SCM_PGND_MASK = actions.ActionCondns.new_flags_and_mask(2)
 AC_NOT_PMIC, AC_PMIC, AC_PMIC_MASK = actions.ActionCondns.new_flags_and_mask(2)
 
 def get_in_pm_pgnd_condns():
@@ -39,9 +35,6 @@ def get_in_pm_pgnd_condns():
         conds = AC_NOT_IN_PM_PGND
     return actions.MaskedCondns(conds, AC_IN_PM_PGND_MASK)
 
-def get_in_scm_pgnd_condns():
-    return actions.MaskedCondns(AC_IN_SCM_PGND if scm.gui.ifce.SCM.in_valid_pgnd else AC_NOT_IN_SCM_PGND, AC_IN_SCM_PGND_MASK)
-
 def get_pmic_condns():
     return actions.MaskedCondns(AC_PMIC if pm.gui.ifce.PM.is_poppable else AC_NOT_PMIC, AC_PMIC_MASK)
 
@@ -50,31 +43,22 @@ def _update_class_indep_pm_pgnd_cb(**kwargs):
     actions.CLASS_INDEP_AGS.update_condns(condns)
     actions.CLASS_INDEP_BGS.update_condns(condns)
 
-def _update_class_indep_scm_pgnd_cb(**kwargs):
-    condns = get_in_scm_pgnd_condns()
-    actions.CLASS_INDEP_AGS.update_condns(condns)
-    actions.CLASS_INDEP_BGS.update_condns(condns)
-
 def _update_class_indep_pmic_cb(**kwargs):
     condns = get_pmic_condns()
     actions.CLASS_INDEP_AGS.update_condns(condns)
     actions.CLASS_INDEP_BGS.update_condns(condns)
 
-enotify.add_notification_cb(enotify.E_CHANGE_WD|scm.E_NEW_SCM, _update_class_indep_scm_pgnd_cb)
 enotify.add_notification_cb(enotify.E_CHANGE_WD|pm.E_NEW_PM, _update_class_indep_pm_pgnd_cb)
 enotify.add_notification_cb(pm.E_PATCH_STACK_CHANGES|pm.E_NEW_PM|enotify.E_CHANGE_WD, _update_class_indep_pmic_cb)
 
 class WDListenerMixin:
     def __init__(self):
-        self.add_notification_cb(enotify.E_CHANGE_WD|scm.E_NEW_SCM, self.scm_pgnd_conds_change_cb)
         self.add_notification_cb(enotify.E_CHANGE_WD|pm.E_NEW_PM, self.pm_pgnd_condns_change_cb)
         self.add_notification_cb(pm.E_PATCH_STACK_CHANGES|pm.E_NEW_PM|enotify.E_CHANGE_WD, self.pmic_condns_change_cb)
-        self.init_action_states()
-    def scm_pgnd_conds_change_cb(self, **kwargs):
-        condns = get_in_scm_pgnd_condns()
-        self.action_groups.update_condns(condns)
+        condn_set = get_in_pm_pgnd_condns() | get_pmic_condns()
+        self.action_groups.update_condns(condn_set)
         try:
-            self.button_groups.update_condns(condns)
+            self.button_groups.update_condns(condn_set)
         except AttributeError:
             pass
     def pm_pgnd_condns_change_cb(self, **kwargs):
@@ -91,17 +75,3 @@ class WDListenerMixin:
             self.button_groups.update_condns(condns)
         except AttributeError:
             pass
-    def init_action_states(self):
-        condn_set = get_in_pm_pgnd_condns() | get_in_scm_pgnd_condns() | get_pmic_condns()
-        self.action_groups.update_condns(condn_set)
-        try:
-            self.button_groups.update_condns(condn_set)
-        except AttributeError:
-            pass
-
-actions.CLASS_INDEP_AGS[actions.AC_DONT_CARE].add_actions(
-    [
-        ("actions_wd_menu", None, _('_Working Directory')),
-        ("actions_quit", Gtk.STOCK_QUIT, _('_Quit'), "",
-         _('Quit'), Gtk.main_quit),
-    ])

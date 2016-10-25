81b799ac7324e8ef1cb03d1e395380ddae7794b8
Peter Williams <pwil3058@gmail.com>
2016-10-25 16:53:15 +1000

    epygibus: add extra functionality for epygibus project
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- text_edit.py
+++ text_edit.py
@@ -13,6 +13,7 @@
 ### along with this program; if not, write to the Free Software
 ### Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+import hashlib
 import shlex
 import os
 import locale
@@ -248,3 +249,53 @@ class DbMessageWidget(MessageWidget):
     def _reload_text_acb(self, *args):
         if self._ok_to_overwrite_summary():
             self.load_text_fm_db()
+
+
+class ModifyUndoSaveBuffer(Gtk.TextBuffer, actions.CBGUserMixin):
+    AC_MODIFIED = actions.ActionCondns.new_flag()
+    AC_MODIFIED_ON_DISK = actions.ActionCondns.new_flag()
+    def __init__(self):
+        Gtk.TextBuffer.__init__(self)
+        actions.CBGUserMixin.__init__(self)
+        self.connect("changed", self._buffer_changed_cb)
+        self.connect("modified-changed", self._buffer_changed_cb)
+    def _buffer_changed_cb(self, _buffer=None):
+        if self.get_modified():
+            self.button_groups.update_condns(actions.MaskedCondns(self.AC_MODIFIED, self.AC_MODIFIED))
+        else:
+            self.button_groups.update_condns(actions.MaskedCondns(0, self.AC_MODIFIED))
+
+class FileModifyUndoSaveBuffer(ModifyUndoSaveBuffer):
+    def __init__(self, file_path=None):
+        self._file_path = file_path
+        self._hash_digest = None
+        ModifyUndoSaveBuffer.__init__(self)
+        if file_path:
+            self.load_file()
+    def load_file(self):
+        with open(self._file_path, "r") as f_obj:
+            text = f_obj.read()
+            self._hash_digest = hashlib.sha1(text).digest()
+            self.set_text(text)
+    def save_file(self):
+        with open(self._file_path, "w") as f_obj:
+            for line in self.get_text(self.get_start_iter(), self.get_end_iter(), True).splitlines(False):
+                f_obj.write(line.rstrip() + os.linesep)
+        self.load_file()
+    def get_current_hash_digest(self):
+        return hashlib.sha1(self.get_text(self.get_start_iter(), self.get_end_iter(), True)).digest()
+    def get_file_hash_digest(self):
+        if not self._file_path or not os.path.isfile(file_path):
+            return None
+        with open(self._file_path, "r") as f_obj:
+            return hashlib.sha1(f_obj.read()).digest()
+    @property
+    def modified_on_disk(self):
+        return self.get_file_hash_digest() != self._hash_digest
+    def _check_file_cb(self, _data):
+        # NB: the view should register this so that its "destroy" signal can be used to deregister it
+        if self.modified_on_disk:
+            self.button_groups.update_condns(actions.MaskedCondns(self.AC_MODIFIED_ON_DISK, self.AC_MODIFIED_ON_DISK))
+        else:
+            self.button_groups.update_condns(actions.MaskedCondns(0, self.AC_MODIFIED_ON_DISK))
+        return True
